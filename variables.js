216a5dca482d20773e6cbc64d254d93a919c663a
Raul1974 <info@planbestudiocreativo.com>
2017-10-26 18:56:09 +0200

    primer commti

---
+++ variables.js
@@ -0,0 +1,11 @@
+//Variables
+
+var dia=10; //Variable INT
+var mes="Noviembre"; //Variable STRIG
+var temperatura=15.5; // Variable FLOAT
+var otoño=true; //Variable BOOLEAN
+
+alert(dia);
+alert(mes);
+alert(temperatura);
+alert(otoño);
